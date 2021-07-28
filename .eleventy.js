946c1a7057bbb1978d45e84ce9bf26c820c12d01
0xNaN <enzodicicco@gmail.com>
2021-07-28 12:38:33 +0200

    copying CNAME to _site

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = (function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('./src/img/');
+  eleventyConfig.addPassthroughCopy('CNAME');
 
   return {
     passthroughFileCopy: true,
